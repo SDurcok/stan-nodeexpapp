fd7556cca149451f601d6044a980431b42804003
Stanislav Durcok <stanislav_durcok@cz.ibm.com>
2020-03-16 16:43:53 +0100

    Updated apps.js to trigger Tekton piplines

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from Stanislav!");
+  res.send("Hello from WAS L2 Org!!!!!!!!!!!!!!!!");
 });
  
 module.exports.app = app;
