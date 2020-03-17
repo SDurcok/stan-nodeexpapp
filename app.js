c7f5bf5f3d62142278fae8edd4a5c374676fe55b
Stanislav Durcok <stanislav_durcok@cz.ibm.com>
2020-03-17 13:55:49 +0100

    Next commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello from WAS L2 Org!!!!!!!!!!!!!");
+  res.send("Hello from WAS L2 session");
 });
  
 module.exports.app = app;
